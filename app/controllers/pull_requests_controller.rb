23a6370c032169526e7d8f5ecb53ee3a5e1aa60d
sudo-infinity <aminchaudhry.dev@gmail.com>
2022-02-05 13:40:56 -0600

    Expand pull request table

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,8 +1,10 @@
 class PullRequestsController < ApplicationController
   def index
+    week_of = params[:week_of].present? ? Date.parse(params[:week_of]) : Time.zone.now
+
     @prs =
       PullRequest
-      .after(Time.zone.now.beginning_of_week)
+      .after(week_of.beginning_of_week)
       .order("org ASC, repo ASC, github_cache->>'created_at' DESC")
   end
 end
